84161859e1019a44792abaf853d2e84fce083963
Bonaventure Anselm <bona.anselm@gmail.com>
2023-03-25 19:52:42 +0100

    Mysql prep script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,14 @@
 -- script that prepares a MySQL server for the project:
+-- Privileges for new user (hbnb_test)
 
--- Log in to MySQL as the root user
-mysql -u root -p
-
--- Create the hbnb_dev_db database if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create the hbnb_dev user if it doesn't already exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+
+-- Set password for the user hbnb_dev
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+USE hbnb_dev_db;
 
 -- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
