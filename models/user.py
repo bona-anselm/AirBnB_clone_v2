bf07a281771c96d74129bc3a0e39768f78c61a96
igraziella <igraziella2@gmail.com>
2023-04-08 16:45:29 -0700

    added user

--- models/user.py
+++ models/user.py
@@ -9,8 +9,13 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = 'users'
-    storage_type == 'db'
-    email = Column(String(128), nullable = False)
-    password = Column(String(128), nullable = False)
-    first_name = Column(String(128), nullable = False)
-    last_name = Column(String(128), nullable = False)
+    if storage_type == 'db':
+        email = Column(String(128), nullable = False)
+        password = Column(String(128), nullable = False)
+        first_name = Column(String(128), nullable = False)
+        last_name = Column(String(128), nullable = False)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
